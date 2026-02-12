b65d6752f442a63ca1174e34ce7cf2ee70e7225e
Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
2026-02-12 21:16:59 +0100

    feat: init varlink interface
    Signed-off-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>

--- src/rpc.rs
+++
@@ -1,8 +0,0 @@
-use crate::config::Settings;
-use std::sync::Arc;
-use tokio::net::UnixListener;
-
-/// Spawn a Varlink server to control the proxy server.
-pub async fn start(settings: Arc<Settings>, listener: UnixListener) -> anyhow::Result<()> {
-    Ok(())
-}
