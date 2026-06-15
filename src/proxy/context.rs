1480347f564527a2a5bb48030f772452970a04aa
Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
2026-06-15 18:41:16 +0200

    proxy/context: make TargetContext derive Debug
    Signed-off-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>

--- src/proxy/context.rs
+++ src/proxy/context.rs
@@ -49,6 +49,7 @@ impl From<fast_socks5::util::target_addr::TargetAddr> for TargetAddr {
     }
 }
 
+#[derive(Debug)]
 pub struct TargetContext {
     pub initial_target: TargetAddr,
     pub resolved_target: Option<TargetAddr>,
